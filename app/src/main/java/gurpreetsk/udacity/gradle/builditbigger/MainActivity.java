0d74124a2e666e940be57cf715ae63735014e92b
Gurpreet <gurpreetsk@hotmail.com>
2016-10-27 21:02:22 +0530

    - free and paid flavors working

--- app/src/main/java/gurpreetsk/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/gurpreetsk/udacity/gradle/builditbigger/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.util.Pair;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,9 +13,6 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
--- app/src/paid/java/gurpreetsk/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/gurpreetsk/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -5,10 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.udacity.gradle.builditbigger.R;
 
 
