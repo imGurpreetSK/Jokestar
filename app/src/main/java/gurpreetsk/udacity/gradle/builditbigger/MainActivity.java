8a2c2e836f26f31c64b140784903b28029a1e2fe
Gurpreet <gurpreetsk@hotmail.com>
2016-10-27 15:53:08 +0530

    - implemented GCE backend

--- app/src/main/java/gurpreetsk/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/gurpreetsk/udacity/gradle/builditbigger/MainActivity.java
@@ -1,22 +1,30 @@
 package gurpreetsk.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.udacity.gradle.builditbigger.R;
 
+import java.io.IOException;
+
 import gurpreetsk.me.Joker;
+import gurpreetsk.me.jokestar.backend.myApi.MyApi;
 import gurpreetsk.me.ohmyjoke_android.JokeActivity;
 
 
 public class MainActivity extends ActionBarActivity {
-
-    Joker joker = new Joker();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,14 +55,56 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view) {
+
+        Joker joker = new Joker();
         String joke = joker.getJoke();
 
-        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
-
-        Intent intent = new Intent(MainActivity.this, JokeActivity.class);
-        intent.putExtra(JokeActivity.JOKE_KEY, joke);
-        startActivity(intent);
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, joke));
 
     }
+
+
+    class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+        private MyApi myApiService = null;
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            if (myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        .setRootUrl("https://sunshine-146209.appspot.com/_ah/api/")
+//                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")        //for emulators
+//                        .setRootUrl("http://192.168.0.10:8080/_ah/api/")      //for deploying on physical device
+                        .setApplicationName(getString(R.string.app_name))
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            context = params[0].first;
+            String joke = params[0].second;
+
+            try {
+                return myApiService.sayHi(joke).execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent intent = new Intent(MainActivity.this, JokeActivity.class);
+            intent.putExtra(JokeActivity.JOKE_KEY, result);
+            startActivity(intent);
+        }
+    }
+
 
 }
